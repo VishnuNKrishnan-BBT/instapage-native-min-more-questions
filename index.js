8816dee3bdde7e3ddab80154cfa74aae25b49da4
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-03-01 16:51:17 +0400

    port corrected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const handler = require('serve-handler');
 const http = require('http');
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const server = http.createServer((request, response) => {
   // You pass two more arguments for config and middleware
