ec8f259e32bbde6e26746f59ee8990b0bc1c1f1e
Ijaz Ali <v-ijaz@damacgroup.com>
2024-03-20 16:24:06 +0400

    insta page changes

--- files/new-staging-script.js
+++ files/new-staging-script.js
@@ -1284,23 +1284,91 @@ startTime = new Date().getTime();
 let defaultCampaignId;
   
 let urlSplits = window.location.href.split("/")
+
 if (urlSplits[urlSplits.length-1].trim().length == 0 ||  urlSplits[urlSplits.length-1][0] == "?") {
   urlSplits = urlSplits[urlSplits.length-2].split("?")[0]
+  console.log("iffffffffff", urlSplits)
 } else {
   urlSplits = urlSplits[urlSplits.length-1].split("?")[0]
+  console.log("elseeeeeeeeee", urlSplits)
 }
+
+// if (urlSplits.includes("gdn")) {
+//   defaultCampaignId = "a1207000000bnOn";
+// } else if (urlSplits.includes("emailer")) {
+//   defaultCampaignId = "a1207000000cX0R";
+// } else if (urlSplits.includes("social")) {
+//   defaultCampaignId = "a121n00000D9pUv";
+// } else if (urlSplits.includes("affilate")) {
+//   defaultCampaignId = "a1207000000d8a4";
+// } else {
+//   defaultCampaignId = "a121n00000Dwy45";
+// }
+console.log("urlSplitsurlSplitsurlSplitsurlSplits", urlSplits)
 
 if (urlSplits.includes("gdn")) {
   defaultCampaignId = "a1207000000bnOn";
-} else if (urlSplits.includes("emailer")) {
+} else if (urlSplits.includes("emailer-specific")) {
   defaultCampaignId = "a1207000000cX0R";
-} else if (urlSplits.includes("social")) {
+} else if (urlSplits.includes("social-specific")) {
   defaultCampaignId = "a121n00000D9pUv";
-} else if (urlSplits.includes("affilate")) {
+} else if (urlSplits.includes("affiliate-specific")) {
   defaultCampaignId = "a1207000000d8a4";
+} else if (urlSplits.includes("social-inf")) {
+  defaultCampaignId = "a121n00000D9pUv";
 } else {
   defaultCampaignId = "a121n00000Dwy45";
 }
+
+
+
+// ======================================== LOGIC OF  ================================================
+// const pageURL = window.location.href
+// const hasUTMParams = pageURL.split('?')[1] !== undefined && pageURL.split('?')[1] !== null
+
+// const addUTMParamsToSessionStorage = () => {
+//     if(!hasUTMParams){
+//         return
+//     }
+
+//     const UTMParamsString = pageURL.split('?')[1]
+//     const UTMParamsArray = UTMParamsString.split('&')
+
+//     if(UTMParamsArray.length == 0){
+//         return
+//     }
+
+//     UTMParamsArray.map(obj => {
+//       sessionStorage.setItem(obj.split('=')[0], obj.split('=')[1])
+//     })
+// }
+
+// const getDefaultCampaignIdAsPerUtmMedium = () => {
+
+//   let campaign_id = sessionStorage.getItem('campaign_id')
+
+//   if(campaign_id) {
+//     return campaign_id
+//   } else {
+//     let utm_medium = sessionStorage.getItem('utm_medium')
+//     if (utm_medium == "gdn") {
+//       return "a1207000000bnOn";
+//     } else if (utm_medium == "emailer-specific") {
+//       return "a1207000000cX0R";
+//     } else if (utm_medium == "social-specific") {
+//       return "a121n00000D9pUv";
+//     } else if (utm_medium == "affiliate-specific") {
+//       return "a1207000000d8a4";
+//     } else if (utm_medium == "social-inf") {
+//       return "a121n00000D9pUv";
+//     } else {
+//       return "a121n00000Dwy45";
+//     }
+//   }
+// }
+
+// defaultCampaignId = getDefaultCampaignIdAsPerUtmMedium()
+// ======================================== END     O N   I N I T ================================================
 
 window.addEventListener("DOMContentLoaded", function () {
   if (window.__featuresReady && window.__featuresReady.indexOf("Form") > -1) {
@@ -1756,4 +1824,6 @@ return $(this).val() ==titleVal;
 
 // ======== O N   I N I T ========
 obtainAccessToken(lqs2clientId, lqs2clientSecret, lqs2tokenEndpoint)
+
+addUTMParamsToSessionStorage()
 // ======== E N D   O F   O N   I N I T ========
