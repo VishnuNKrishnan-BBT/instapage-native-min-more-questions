68fca19ee90ef71a015805aac5c527603799a4ee
Ijaz Ali <v-ijaz@damacgroup.com>
2024-04-01 11:28:59 +0400

    bff access token

--- files/new-staging-script.js
+++ files/new-staging-script.js
@@ -23,8 +23,10 @@ const lqs2leadEndpoint = "https://uat-mashery.damacgroup.com/v1/lqs/redis";
 
 
 // ======== BFF   C O N F I G ========
-const bffEmail = "user@example.com";
-const bffPassword = "string";
+const username = "DigitalIntegration";
+const bffEmail = "Digital.Integration@damacgroup.com";
+const bffPassword = "Clj~BEh![;)AL";
+const bffBaseUrlStage = 'https://stg-api.damacproperties.com/' 
 
 // Function to retrieve the token from sessionStorage
 function getTokenFromLocalStorage() {
@@ -33,29 +35,51 @@ function getTokenFromLocalStorage() {
     if(accessTokenForBff) {
       return accessTokenForBff
     } else {
-      bffLayerTokenAccess()
+      return bffLayerTokenAccess() // Return the promise
     }
   }
   return;
 }
-function bffLayerTokenAccess() {
-  fetch('https://dummyjson.com/auth/login', {
+
+async function bffLayerTokenAccess() {
+  return fetch(`${bffBaseUrlStage}/get-token`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      
-      username: 'kminchelle',
-      password: '0lelplR',
+      email: bffEmail,
+      password: bffPassword,
     })
   })
   .then(res => res.json())
   .then((res)=>{
     console.log("res", res)
     window.sessionStorage.setItem('accessTokenForBff', res?.token)
+    return res.token; // Return the token
   });
 }
 
-getTokenFromLocalStorage()
+// Function to fetch pricing data
+// function fetchPricingData(drupleId) {
+//   return getTokenFromLocalStorage().then(token => {
+//     return fetch(`${bffBaseUrlStage}/instapage/get-pricing/${drupleId}`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${token}`
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log('Pricing Data:', data);
+//       // Handle pricing data here
+//     })
+//     .catch(error => console.error('Error fetching pricing data:', error));
+//   });
+// }
+
+// // Example usage:
+// const drupleId = 'your-id';
+// fetchPricingData(drupleId);
 // ======== BFF   C O N F I G ========
 
 
