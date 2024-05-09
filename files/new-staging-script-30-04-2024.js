48feb7c2c2ff336cefc23cef6f837a9ff5b35664
Ijaz Ali <v-ijaz@damacgroup.com>
2024-05-09 14:00:24 +0400

    in arabic language phone field name change

--- files/new-staging-script-30-04-2024.js
+++ files/new-staging-script-30-04-2024.js
@@ -225,13 +225,15 @@ document.addEventListener("DOMContentLoaded", async function () {
     _Translate.set("domain starts with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªØ¨Ø¯Ø£ Ø¨Ù†Ù‚Ø·Ø©");
     _Translate.set("domain is not valid", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØºÙŠØ± ØµØ­ÙŠØ­Ø©");
     _Translate.set("domain ends with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªÙ†ØªÙ‡ÙŠ Ø¨Ù†Ù‚Ø·Ø©");
-    phoneInput = "Ù‡Ø§ØªÙ";
+    phoneInput = "هاتف";
     let data = $('input[name="' + phoneInput + '"]');
+    console.log("datadatadatadata", data)
     if (data.length > 0) {
-      phoneInput = "Ù‡Ø§ØªÙ";
+      phoneInput = "هاتف";
     } else {
-      phoneInput = "Ø§Ù„Ù‡Ø§ØªÙ";
+      phoneInput = "هاتف";
     }
+    console.log("datadatadatadata", phoneInput)
     //console.log("data...", data);
     titleInput = "Ø§Ù„Ù„Ù‚Ø¨";
     firstNameInput = "Ø§Ù„Ø§Ø³Ù… Ø§Ù„Ø£ÙˆÙ„";
