6fcf13d70a44c3b564948586a83290940aa85110
Ijaz Ali <v-ijaz@damacgroup.com>
2024-05-14 13:55:22 +0400

    phone number field is not showing due to lang issues

--- files/new-staging-script.js
+++ files/new-staging-script.js
@@ -1029,10 +1029,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     preferredLanguageInput = "Preferred Language";
 
     findAJob="Find a job"
-  }
 
-  if (websiteLanguage == "EN" || websiteLanguage == "FR" || websiteLanguage == "SP" || websiteLanguage == "HE" || websiteLanguage == "CH" || websiteLanguage == "IT" || websiteLanguage == "RU" || websiteLanguage == "GK" || websiteLanguage == "GR" || websiteLanguage == "NL" || websiteLanguage == "RO" || websiteLanguage == "TR") {
-    phoneInput = "Phone";
     let data = $('input[name="' + phoneInput + '"]');
     if (data.length > 0) {
       phoneInput = "هاتف";
