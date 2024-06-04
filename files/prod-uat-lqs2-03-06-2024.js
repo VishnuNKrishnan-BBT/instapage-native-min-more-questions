b244edfcca151a7440f822edcbf126b1a30b0eae
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-06-04 12:55:41 +0400

    Arabic phoneInput correction

--- files/prod-uat-lqs2-03-06-2024.js
+++ files/prod-uat-lqs2-03-06-2024.js
@@ -225,12 +225,12 @@ document.addEventListener("DOMContentLoaded", async function () {
     _Translate.set("domain starts with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªØ¨Ø¯Ø£ Ø¨Ù†Ù‚Ø·Ø©");
     _Translate.set("domain is not valid", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØºÙŠØ± ØµØ­ÙŠØ­Ø©");
     _Translate.set("domain ends with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªÙ†ØªÙ‡ÙŠ Ø¨Ù†Ù‚Ø·Ø©");
-    phoneInput = "Ù‡Ø§ØªÙ";
+    phoneInput = "Ã™â€¡Ã˜Â§Ã˜ÂªÃ™Â";
     let data = $('input[name="' + phoneInput + '"]');
     if (data.length > 0) {
-      phoneInput = "Ù‡Ø§ØªÙ";
+      phoneInput = "Ã™â€¡Ã˜Â§Ã˜ÂªÃ™Â";
     } else {
-      phoneInput = "Ø§Ù„Ù‡Ø§ØªÙ";
+      phoneInput = "Ã˜Â§Ã™â€žÃ™â€¡Ã˜Â§Ã˜ÂªÃ™Â";
     }
     //console.log("data...", data);
     titleInput = "Ø§Ù„Ù„Ù‚Ø¨";
