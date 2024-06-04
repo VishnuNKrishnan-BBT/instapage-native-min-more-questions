998c6e3e7348b539e14098842b65f5fd9c4ae586
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-06-04 12:59:45 +0400

    Arabic phoneInput correction

--- files/prod-uat-lqs2-03-06-2024.js
+++ files/prod-uat-lqs2-03-06-2024.js
@@ -225,12 +225,12 @@ document.addEventListener("DOMContentLoaded", async function () {
     _Translate.set("domain starts with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªØ¨Ø¯Ø£ Ø¨Ù†Ù‚Ø·Ø©");
     _Translate.set("domain is not valid", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØºÙŠØ± ØµØ­ÙŠØ­Ø©");
     _Translate.set("domain ends with a dot", "Ø§Ù„Ù…Ø¹Ù„ÙˆÙ…Ø§Øª Ø¨Ø§Ù„Ø®Ø§Ù†Ø© ØªÙ†ØªÙ‡ÙŠ Ø¨Ù†Ù‚Ø·Ø©");
-    phoneInput = "رقم الهاتف";
+    phoneInput = "هاتف";
     let data = $('input[name="' + phoneInput + '"]');
     if (data.length > 0) {
-        phoneInput = "رقم الهاتف";
+        phoneInput = "هاتف";
     } else {
-        phoneInput = "رقم الهاتف";
+        phoneInput = "هاتف";
     }
     //console.log("data...", data);
     titleInput = "Ø§Ù„Ù„Ù‚Ø¨";
