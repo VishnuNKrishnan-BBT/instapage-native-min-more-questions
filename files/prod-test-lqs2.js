267521eae71b3efa85de8dae242162cab91e382f
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-08-06 10:51:46 +0400

    removed commented code

--- files/prod-test-lqs2.js
+++ files/prod-test-lqs2.js
@@ -16,9 +16,6 @@ const lqs1authKey = "newiuw3ujdjudqoeneoie1E";
 
 // ======== L Q S 2.0   C O N F I G ========
 const lqs2clientId = "instapage_user";
-// const lqs2clientSecret = "zAx7jC4brSR9gxVBys6skutRnGeFzxVBys6skutRnGeFzdgdZ8";
-// const lqs2tokenEndpoint = "https://uat-mashery.damacgroup.com/v1/oauth/token";
-// const lqs2leadEndpoint = "https://uat-mashery.damacgroup.com/v1/lqs/redis";
 const lqs2clientSecret = "IAx7jC4brSR9gxVBys6ys6skutRnGeFzdgdZ8skutRnGeFzxVB";
 const lqs2tokenEndpoint = "https://api.damacgroup.com/lqs-api/v1/token";
 const lqs2leadEndpoint = "https://api.damacgroup.com/lqs/v1/getdata";
