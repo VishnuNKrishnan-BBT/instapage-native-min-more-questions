b7cae94fd7df20c0218c635ec17aa15c00c04eeb
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-08-29 15:48:29 +0400

    correction

--- files/ypoValidtion.js
+++ files/ypoValidtion.js
@@ -33,8 +33,8 @@ const addFieldValidation = (field, allowedArray) => {
             e.preventDefault()
         }
     
-        if(isValid(e.key, allowedArray)){
-            field.value += e.key
+        if(isValid(e.target.value, allowedArray)){
+            field.value += e.target.value
         }
     })
 }
