a548788fd882fa5bbcc568c7d35e8a0aabb8e97b
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-08-30 17:22:05 +0400

    email validation added

--- files/ypoValidation.js
+++ files/ypoValidation.js
@@ -7,6 +7,15 @@ const alphaSpace = [
 ]
 const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const alphaNumSpaceSpec = [...alphaSpace, ...num, `'`, `:`, `-`, `_`, `&`, `"`]
+const allowedEmailCharacters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '.', '_', '%', '+', '-', '@'
+]
+
 
 const nameInput = Array.from(document.getElementsByTagName('form'))[0][0]
 const cityInput = Array.from(document.getElementsByTagName('form'))[0][1]
@@ -15,6 +24,21 @@ const emailInput = Array.from(document.getElementsByTagName('form'))[0][3]
 const ypocInput = Array.from(document.getElementsByTagName('form'))[0][4]
 
 function handleInput(event, allowedChars) {
+    const field = event.target
+    const value = field.value
+    let filteredValue = ''
+
+    for (let char of value) {
+        if (allowedChars.includes(char)) {
+            filteredValue += char
+        }
+    }
+
+    // Update the input field value with the filtered value
+    field.value = filteredValue
+}
+
+function handleEmailInput(event, allowedChars) {
     const field = event.target
     const value = field.value
     let filteredValue = ''
@@ -38,6 +62,9 @@ const initValidation = () => {
     })
     phoneInput.addEventListener('input', e => {
         handleInput(e, num)
+    })
+    emailInput.addEventListener('input', e => {
+        handleInput(e, allowedEmailCharacters)
     })
     ypocInput.addEventListener('input', e => {
         handleInput(e, alphaNumSpaceSpec)
