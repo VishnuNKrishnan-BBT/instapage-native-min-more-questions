31e72efc5e866abd0770113a8a68783732abbd92
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-09-16 10:54:59 +0400

    title update for country code field

--- files/ypoValidtion2.js
+++ files/ypoValidtion2.js
@@ -13,7 +13,7 @@ const cityInput = Array.from(document.getElementsByTagName('form'))[0][1]
 const phoneInput = Array.from(document.getElementsByTagName('form'))[0][2]
 const emailInput = Array.from(document.getElementsByTagName('form'))[0][3]
 const ypocInput = Array.from(document.getElementsByTagName('form'))[0][4]
-const countryCode = Array.from(document.getElementsByTagName('form'))[0][5]
+const countryCodeInput = Array.from(document.getElementsByTagName('form'))[0][5]
 
 const isValid = (char, validChars) => {
         if (!validChars.includes(char)) {
