4e935c7b769e4bd23a0ee3b5bd5cf7e705621426
Vishnu Navaneeth Krishnan <v-vishnuk@damacgroup.com>
2024-09-24 11:23:25 +0400

    Keyword fix added

--- files/prod-test-lqs2.js
+++ files/prod-test-lqs2.js
@@ -1897,6 +1897,9 @@ window.addEventListener("DOMContentLoaded", function () {
         //Hardcoding Lead_Gen_Identifier based on request from Digital Marketing
         data.Lead_Gen_Identifier = 'instapage'
 
+        //Decoding keyword value - Requested by Jabez on 20-09-2024. No Ticket.
+        data.keyword = decodeURIComponent(data.keyword)
+
         //Disable submit buttons
         if(formValid.isValid() && (sendToLQS1 || sendToLQS2)){
           toggleSubmitBtns('disable')
